789976f6872e3f24d7f708d9193e5b456c5f184c
Nnabugwu Emmanuel Chiemezie <81854865+nnabugwu24@users.noreply.github.com>
2026-08-12 14:33:26 +0100

    Include external scripts for ads and tracking
    Added Google Ads script and other external scripts to the page.

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 import React from "react";
 import Header from "./components/Header";
 import WhatsAppButton from "./components/WhatsAppButton";
-
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8905718966138726"
+     crossorigin="anonymous"></script>
 export default function () {
   return (
    
 
     <div className="flex flex-col h-vh w-100% p-10 bg-gray-200 gap-10" >
       <Header />
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8905718966138726"
+     crossorigin="anonymous"></script>
       <script src="https://pl30812079.effectivecpmnetwork.com/9a/4e/de/9a4ede3807023555efba120805133190.js"></script>
 
     <script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>
